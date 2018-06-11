4cb9f5a93af2c5290f1a69d16fb9d1039cc4cafb
sdcoffey <stevendcoffey@gmail.com>
2018-06-10 22:53:29 -0400

    add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	"extends": [
+		"plugin:react/recommended",
+		"eslint:recommended"
+	],
+	"plugins": [
+		"react",
+		"react-native"
+	],
+  "parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true,
+      "modules": true
+		},
+	},
+  "rules": {
+    "react/prop-types": 0
+  }
+};
